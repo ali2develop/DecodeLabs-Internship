c23daaac4822aba20d674bde04a317e32dcf1d26
ali2develop <muhammadaliamaan515@gmail.com>
2026-07-15 10:27:21 +0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementById('theme-toggle');
-
-toggleButton.addEventListener('click', () => {
-    // This tells the body to add or remove the 'dark-mode' class
-    document.body.classList.toggle('dark-mode');
-});
